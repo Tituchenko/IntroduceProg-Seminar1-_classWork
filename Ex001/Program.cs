7574d47b07bd263314eee4aa2e42b6cde964ce6e
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-16 14:34:24 +0300

    Add bibliotecs math metod

--- Ex001/Program.cs
+++ Ex001/Program.cs
@@ -5,4 +5,9 @@ int number=int.Parse(Console.ReadLine());
 // Квадрат математически
 int sqr=number*number;
 
-Console.WriteLine ($"Кварат {number} равен {sqr}");
+Console.WriteLine ($"Квадрат {number} равен {sqr}");
+
+// Метод из библиотеки math
+
+int sqr1=Convert.ToInt32(Math.Pow(number,2));
+Console.WriteLine ($"Метод из библитеки:Квадрат {number} равен {sqr}");
