58f1b2f82c612da575b93d08b1943d14f1bd8340
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-16 14:25:31 +0300

    add Ex 001 sqared number (mathematics)

---
+++ Ex001/Program.cs
@@ -0,0 +1,8 @@
+﻿// Написать программу которая на вход принимает число и выдает ее квадрат
+Console.WriteLine ("Введи число ");
+int number=int.Parse(Console.ReadLine());
+
+// Квадрат математически
+int sqr=number*number;
+
+Console.WriteLine ($"Кварат {number} равен {sqr}");
