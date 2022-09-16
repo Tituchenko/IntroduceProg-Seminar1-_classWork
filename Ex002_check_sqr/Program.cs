a4a18c2bad54ac2d5d34b98c4e163ec951a076c5
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-16 14:50:43 +0300

    add Ex 002 Check SQR a and b

---
+++ Ex002_check_sqr/Program.cs
@@ -0,0 +1,16 @@
+﻿// Ввести два числа, проверить является ли второе квадратом первого
+Console.Write ("Введите первое число: ");
+int a=int.Parse(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int b=int.Parse(Console.ReadLine());
+
+int sqrA=Convert.ToInt32(Math.Pow(a,2));
+
+if (b==sqrA)
+{
+    Console.WriteLine ($"Второе число {b} является квадратом первого {a}");
+}
+else
+{
+    Console.WriteLine ($"Второе число {b} не является квадратом первого {a} (его квадрат = {sqrA})");
+}
