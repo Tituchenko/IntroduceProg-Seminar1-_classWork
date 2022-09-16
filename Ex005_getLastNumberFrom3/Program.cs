f1794048afc83eea168218c68ccfffc9f7b9db49
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-16 16:07:14 +0300

    Ex 005 optimazed

--- Ex005_getLastNumberFrom3/Program.cs
+++ Ex005_getLastNumberFrom3/Program.cs
@@ -1,6 +1,6 @@
 ﻿// На входе получает трехзначное число, на выходе последнюю цифру
 Console.Write ("Введите трехзначное число: ");
 int number=int.Parse(Console.ReadLine());
-int thirdnumber=number%100%10; //третья цифра
+int thirdnumber=number%10; //третья цифра
 
 Console.WriteLine ($"Послденяя цифра числа {number} это {thirdnumber}");
