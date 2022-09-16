1840d6d0bcdc86a1b3caada13e8c16f6968be071
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-16 15:59:23 +0300

    refactoring Ex 007 - used %

--- Ex005_getLastNumberFrom3/Program.cs
+++ Ex005_getLastNumberFrom3/Program.cs
@@ -1,7 +1,6 @@
 ﻿// На входе получает трехзначное число, на выходе последнюю цифру
 Console.Write ("Введите трехзначное число: ");
 int number=int.Parse(Console.ReadLine());
-int firstNumber=number/100; //первая цифра
-int secondNumber=(number-firstNumber*100)/10; // вторая цифра
-int thirdnumber=number-firstNumber*100-secondNumber*10; //третья цифра
+int thirdnumber=number%100%10; //третья цифра
+
 Console.WriteLine ($"Послденяя цифра числа {number} это {thirdnumber}");
