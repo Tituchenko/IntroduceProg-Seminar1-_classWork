825364a00c3806a02b4d897a61a44ddc5c067880
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-16 15:38:16 +0300

    add Ex 004 from -N to N

---
+++ Ex004_from-N_To_N/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу, которая принимает N и показывает все числа от -N до N
+
+Console.Write ("Введите N:");
+int N=int.Parse(Console.ReadLine());
+
+int i=-N;
+
+while (i<=N)
+{
+    Console.WriteLine(i);
+    i++;
+}
